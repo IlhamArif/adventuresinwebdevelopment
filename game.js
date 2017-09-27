7bbe8acd55a1a9ed4d3360aee0f2cf05112ed0a0
Syed Muhd Ilham Arif <sm.ilhamarif@gmail.com>
2017-09-27 19:59:43 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,4 +1,12 @@
 function begin(e) {
-alert(e.keyCode);
+  if (e.keyCode == 13) {
+  var code = '<textarea cols=190 rows=40 id="code"></textarea><pre><button onclick=nles()>Run</button>   <button onclick=stins()>Repeat Instructions</button></pre>'
+  document.body.innerHTML = code;
+  stins()
+  }
+}
+function stins() {
+alert("Mike: Alright, Follow These Instructions.")
+alert("")
 }
 document.onkeydown = begin;
