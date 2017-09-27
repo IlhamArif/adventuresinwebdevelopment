2e9feaeeff2608957d89f12624e0e0992781aea7
Syed Muhd Ilham Arif <sm.ilhamarif@gmail.com>
2017-09-27 23:22:19 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function begin(e) {
   if (e.keyCode == 13) {
-  var code = '<textarea cols=190 rows=40 id="code"></textarea><pre><button onclick=nles()>Run</button>   <button onclick=stins()>Repeat Instructions</button></pre>'
+  var code = '<textarea cols=141 rows=30 id="code"></textarea><pre><button onclick=nles()>Run</button>   <button onclick=stins()>Repeat Instructions</button></pre>'
   document.body.innerHTML = code;
   stins()
   }
