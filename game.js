987d0eab9d5d02873a9f41d408507438fe4d02b1
Syed Muhd Ilham Arif <sm.ilhamarif@gmail.com>
2017-09-27 23:21:12 +0800

    Update game.js

--- game.js
+++ game.js
@@ -9,5 +9,7 @@ function stins() {
 alert("Mike: Alright, Follow These Instructions.")
 alert("Mike: First Of All, Let's create a Hello World program.\n      1.Create an alert box by using 'alert()'.\n      2.Inside the alert statement, insert 2 quotation marks.\n      3.Then, inside the quotation marks, insert the phrase 'Hello, World!' and click Run")
 }
-function nles() {}
+function nles() {
+var allcode = document.getElementById("").value;
+}
 document.onkeydown = begin;
