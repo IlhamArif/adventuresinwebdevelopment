67aa53ee2041746f46880b724b7e28c55b79518a
Syed Muhd Ilham Arif <sm.ilhamarif@gmail.com>
2017-09-30 22:23:38 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,23 +1,3 @@
 function begin() {
 alert("Mike: Bro, have you heard? Mozilla is having job openings for web deveopers who have experience \n      in JavaScript.\nJoe(You): I don't know, I never coded in JavaScript.\nMike: Don't Worry, let me help you.")
-var code = '<textarea cols=141 rows=30 id="code"></textarea><pre><button onclick=nxles()>Run</button>   <button onclick=ndins()>Repeat Instructions</button></pre>'
-document.body.innerHTML = code;
-stins()
-}
-function stins() {
-alert("Mike: Alright, Follow These Instructions.")
-alert("Mike: First Of All, Let's create a Hello World program.\n      1.Create an alert box by using 'alert()'.\n      2.Inside the alert statement, insert 2 quotation marks.\n      3.Then, inside the quotation marks, insert the phrase 'Hello, World!' and click Run")
-}
-function nles() {
-var allcode = document.getElementById("code").value;
-if (allcode == 'alert("Hello, World!")') {
-var s = document.createElement("script")
-s.async = true;
-s.innerHTML = allcode;
-document.body.appendChild(s);
-alert("Mike: Good Bro, It's Cool.")
-var code = '<textarea cols=141 rows=30 id="code"></textarea><pre><button onclick=nxles()>Run</button>   <button onclick=ndins()>Repeat Instructions</button></pre>'
-document.body.innerHTML = code;
-} else {
-alert("Mike: Bro, you're doing it wrong.\n      Try it again.")
 }
