56d60005d4b3f0b8cf1ba9b2f6e0e9b9e93cb7ab
Syed Muhd Ilham Arif <sm.ilhamarif@gmail.com>
2017-10-01 22:36:29 +0800

    Update game.js

--- game.js
+++ game.js
@@ -5,7 +5,7 @@ stins()
 }
 function stins() {
 alert("Mike: Alright, first of all let's make a hello world program.")
-alert("MIKe: Firstly, type alert() in the textarea.\nMike: Then, put two quotation marks in the parentheses.\nMike: Finally, insert Hello, World! and press run.")
+alert("Mike: Firstly, type alert() in the textarea.\nMike: Then, put two quotation marks in the parentheses.\nMike: Finally, insert 'Hello, World!' and press run.")
 }
 function strun() {
 var stcode = document.getElementById("code").value
@@ -15,6 +15,7 @@ s.async = true;
 s.innerHTML = stcode;
 document.body.appendChild(s);
 alert("Mike: Good Job Joe.")
+alert("Mike: Now, let's learn about prompts and how to get user input.")
 document.body.innerHTML = '<textarea id="code" cols=141 rows=30></textarea><br /><pre><button onclick=ndrun()>Run</button>   <button onclick=ndins()>Repeat Instructions</button></pre>'
 ndins()
 } else {
@@ -23,5 +24,26 @@ s.async = true;
 s.innerHTML = stcode;
 document.body.appendChild(s);
 alert("Mike: Sorry Bro, you did something wrong.") 
+}
+}
+function ndins() {
+alert("Mike: Now, type prompt() in the textarea.\nMike: Then, put two quotation marks in the parentheses.\nMike: Lastly, insert 'What is your name?' and press run.")
+}
+function ndrun() {
+var ndcode = document.getElementById("code").value;
+if (ndcode == 'prompt("What is your name?")') {
+var s = document.createElement("script")
+s.async = true;
+s.innerHTML = ndcode;
+document.body.appendChild(s);
+alert("Mike: Good Job Joe.")
+alert("Mike: Now, let's learn about variables and how to create them")
+document.body.innerHTML = '<textarea id="code" cols=141 rows=30></textarea><br /><pre><button onclick=ndrun()>Run</button>   <button onclick=ndins()>Repeat Instructions</button></pre>'
+rdins()
+} else {
+s.async = true;
+s.innerHTML = ndcode;
+document.body.appendChild(s);
+alert("Mike: Sorry Bro, you did something wrong.")
 }
 }
